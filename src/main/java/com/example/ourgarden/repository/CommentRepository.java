506aea05cc79d7fd9f32919453667ef6eeaca683
PKosev <pavel.kosev.14@gmail.com>
2021-11-20 12:25:52 +0200

    addOrder/addCommentInfoForLater/someLogicalProblems

---
+++ src/main/java/com/example/ourgarden/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.example.ourgarden.repository;
+
+import com.example.ourgarden.model.entity.CommentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+}
